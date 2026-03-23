2244afac638dfa735ed0e7516a830e491cf45a2a
Rofik Bhi <72003761+Rofikali@users.noreply.github.com>
2026-03-23 05:15:10 +0000

    working this almost ./imgengine_cli \   --input input.jpg \   --cols 6 \   --rows 1 \   --gap 10 \   --width 3.5 \   --height 3.0 \   --padding 10 just cows and cols something problem

---
+++ imgengine/build/CMakeFiles/imgengine.dir/src/layout/auto_fit.c.o.d
@@ -0,0 +1,7 @@
+CMakeFiles/imgengine.dir/src/layout/auto_fit.c.o: \
+ /workspaces/imgengine/imgengine/src/layout/auto_fit.c \
+ /usr/include/stdc-predef.h \
+ /workspaces/imgengine/imgengine/include/imgengine/layout.h \
+ /workspaces/imgengine/imgengine/include/imgengine/image.h \
+ /workspaces/imgengine/imgengine/include/imgengine/memory_pool.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h
---
+++ imgengine/build/CMakeFiles/imgengine_cli.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for imgengine_cli.
+# This may be replaced when dependencies are built.
